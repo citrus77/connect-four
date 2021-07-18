b431b5b2c3dbe769c044e24fddab84ec3529d467
darrian77 <darrian@outlook.com>
2021-07-17 19:22:47 -0700

    checkWin mostly working (the hard way)

--- js/app.js
+++ js/app.js
@@ -18,12 +18,12 @@ const resetState = () => {
   state.currentPlayerIdx = 0;
   state.getCurrentPlayer = () => state.playerNames[state.currentPlayerIdx];
   state.inProgress = false;
+  state.isDraw = false;
   state.p1Turn = true;
   state.p2Turn = false;
   state.vsCPU = false;
   state.playerMove = 'p1move';
   state.scores = [0, 0];
-  // console.log(state.board);
 };
 
 
@@ -32,7 +32,8 @@ const boardElem = document.querySelector('#board');
 const playerTurnElem = document.querySelector('#player-turn');
 const scoreElem1 = document.querySelector('#score-p1');
 const scoreElem2 = document.querySelector('#score-p2');
-// const playerPosElem = document.querySelector('data-coordinates')
+const resetElem = document.querySelector('#reset')
+const restartElem = document.querySelector('#restart')
 
 
 // ***************** DOM MANIPULATION FUNCTIONS *****************
@@ -51,26 +52,69 @@ const renderBoard = () => {
   };
 };
 
+//Render player name inputs and game message
+const renderPlayer = () => {
+  let text;
+  if (!state.playerNames[0] || !state.playerNames[1]) {
+    text = `
+      <input name='player1' class='p1name' placeholder='Enter Player 1'>
+      <input name='player2' class='p2name' placeholder='Enter Player 2'>
+      <button class='start'>GO!</button>
+    `
+  }
+  else if (state.isDraw) {    
+    text = `
+      <span class='game-over'>The game is a draw!</span>   
+      <button class='reset'>Play Again!</button>
+      <button class='restart'>Restart Game</button>
+    `
+  }
+  else if (!state.isDraw && !state.inProgress) {
+    text = `
+    <span class='game-over'>${state.getCurrentPlayer()} wins!</span> 
+    <button class='reset'>Play Again!</button>
+    <button class='restart'>Restart Game</button>
+    `
+  }
+  else {
+    text = `It's currently ${state.getCurrentPlayer()}'s turn.`;
+  }    
+  playerTurnElem.innerHTML = text;
+};
+
+const renderScore = () => {
+  scoreElem1.innerHTML = `
+    <div class='score'>P1</div>
+    <div class='score'>${state.playerNames[0]}</div>
+    <div class='score'>Wins: ${state.scores[0]}</div>    
+  `;
+  scoreElem2.innerHTML = `
+    <div class='score'>P2</div>
+    <div class='score'>${state.playerNames[1]}</div>
+    <div class='score'>Wins: ${state.scores[1]}</div>    
+  `;
+};
 
 //Make a move and change turns
 const playerMove = (event) => {
   const target = event.target
-  if (event.target.className === 'circle' && state.inProgress
-    && state.p1Turn) {
+  if (event.target.className === 'circle' && state.inProgress && state.p1Turn) {
     dropInCol(event);
+    checkWon();
     state.p1Turn = false;
     state.p2Turn = true;
     state.playerMove = 'p2move'
     state.currentPlayerIdx = 1;
   }
-  else if (event.target.className === 'circle' && state.inProgress
-    && state.p2Turn && !state.vsCPU) {
+  else if (event.target.className === 'circle' && state.inProgress && state.p2Turn && !state.vsCPU) {
     dropInCol(event);
+    checkWon();
     state.p1Turn = true;
     state.p2Turn = false;
     state.playerMove = 'p1move'
     state.currentPlayerIdx = 0;
   }
+ 
   //UNCOMMENT BELOW AFTER FIGURING OUT VERSUSCPU FUNCTION
   /*
   else if (event.target.className === 'circle' && p2Turn && !p1Turn && vsCPU) {
@@ -85,61 +129,65 @@ const playerMove = (event) => {
   render()
 };
 
-
-//Render player name inputs and game message
-const renderPlayer = () => {
-  let text;
-  if (!state.playerNames[0] || !state.playerNames[1]) {
-    text = `
-      <input name="player1" class="p1name" placeholder="Enter Player 1">
-      <input name="player2" class="p2name" placeholder="Enter Player 2">
-      <button class="start">GO!</button>
-    `
-  } else {
-    text = `It's currently ${state.getCurrentPlayer()}'s turn.`;
-  }
-  playerTurnElem.innerHTML = text;
-};
-
-const renderScore = () => {
-  scoreElem1.innerHTML = `
-    <div class="score">P1</div>
-    <div class="score">${state.playerNames[0]}</div>
-    <div class="score">Wins: ${state.scores[0]}</div>    
-  `;
-  scoreElem2.innerHTML = `
-    <div class="score">P2</div>
-    <div class="score">${state.playerNames[1]}</div>
-    <div class="score">Wins: ${state.scores[1]}</div>    
-  `;
-};
-
-
 // ************** GAME LOGIC FUNCTIONS **********
 const dropInCol = (event) => {
   const checkElem = event.target;
   const [y, x] = checkElem.dataset.coordinates.split(',');
-  for (let rowIdx = 0; rowIdx < state.board.length; rowIdx++) {
+  for (let rowIdx = 0; rowIdx < 6; rowIdx++) {
     if (state.board[rowIdx][x] !== 'circle') {
       state.board[rowIdx - 1][x] = state.playerMove;
       return;
     }
     else if (rowIdx === 5) {
       state.board[rowIdx][x] = state.playerMove;
+      return;
     };
   };
 };
 
-const checkWin = () => {
+//Functions to check when a player wins
+//If all spots are taken and none have 'circle' class declare draw
+const checkDraw = () => {
+  let count = 0
+  for (let y = 0; y < state.board.length; y++) {
+    let rows = state.board[y];
+    for (let x = 0; x < rows.length; x++){
+      let cols = rows[x];    
+      if (cols.includes('circle') || rows.includes('circle')) count++;
+      if (count === 0) {
+        return true;   
+      };
+    };         
+  };
+  return;
+};
+
+// const versusCPU = () => {
   
+// };
+
+const checkWon = () => {
+  if (state.currentPlayerIdx === 0) checkP1Win();
+  if (state.currentPlayerIdx === 1) checkP2Win();
+  //If player 1 wins
+  if (checkP1Win()) {
+    state.inProgress = false;
+    state.scores[0]++;    
+    console.log('Player1 Wins')
+  };
+
+  if (checkP2Win()) {
+    state.inProgress = false;
+    state.scores[1]++;
+    console.log('Player2 Wins')
+  };
+
+  if (checkDraw()) {    
+    state.inProgress = false;
+    state.isDraw = true;
+    console.log(`it's a draw`)
+  };
 };
-
-/*
-const versusCPU = () => {
-
-};
-*/
-
 
 // ************** HELPER FUNCTIONS **************
 const render = () => {
@@ -147,6 +195,7 @@ const render = () => {
   renderBoard();
   renderPlayer();
 };
+
 
 const startGame = (event) => {
   if (event.target.className !== 'start') {
@@ -171,28 +220,35 @@ const startGame = (event) => {
   render();
 };
 
-const isTaken = ([rowIdx, x]) => {
-  if ([y, x] === 'p1move' || [y][x] === 'p2move') return true;
+const gameOver = () => {
+  state.inProgress = false;
+}
+
+const resetBoard = () => {
+  let scores = state.scores;
+  let playerNames = state.playerNames;
+  resetState();
+  render()
+  state.scores = scores;
+  state.playerNames = playerNames;  
+};
+
+const restartGame = () => {
+  let scores = state.scores;
+  let playerNames = state.playerNames;
+  resetState();
+  state.scores = scores;
+  state.playerNames = playerNames;
 }
 
 
-//SHUFFLE FUNCTION BORROWED FROM;
-//https://bost.ocks.org/mike/shuffle
-const shuffle = (array) => {
-  let m = array.length, t, i;
-  while (m) {
-    i = Math.floor(Math.random() * m--);
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
-  };
-  return array;
-};
-
 // **************** EVENT LISTERNERS *****************
 board.addEventListener('click', playerMove);
-
 playerTurnElem.addEventListener('click', startGame);
+// playerTurnElem.addEventListener('click', function (event) {
+//   if (event.target.className === 'reset') resetBoard();
+// })
+// playerTurnElem.addEventListener('click', restartGame)
 
 // ***************** BOOTSTRAPPING *****************
 resetState();
